a72e2d6914a0f044d97fcc06a51c008a9d9ffca6
CitraYS <citra.yunela@gmail.com>
2026-02-09 23:24:42 +0700

    wip

---
+++ models/DataForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "data_form".
+ *
+ * @property int $id_form_data
+ * @property int|null $id_form
+ * @property int|null $id_registrasi
+ * @property string|null $data
+ * @property bool|null $is_delete
+ * @property int|null $create_by
+ * @property int|null $update_by
+ * @property string|null $create_time_at
+ * @property string|null $update_time_at
+ */
+class DataForm extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'data_form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id_form', 'id_registrasi', 'data', 'create_by', 'update_by'], 'default', 'value' => null],
+            [['is_delete'], 'default', 'value' => 0],
+            [['id_form_data'], 'required'],
+            [['id_form_data', 'id_form', 'id_registrasi', 'create_by', 'update_by'], 'default', 'value' => null],
+            [['id_form_data', 'id_form', 'id_registrasi', 'create_by', 'update_by'], 'integer'],
+            [['data', 'create_time_at', 'update_time_at'], 'safe'],
+            [['is_delete'], 'boolean'],
+            [['id_form_data'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_form_data' => 'Id Form Data',
+            'id_form' => 'Id Form',
+            'id_registrasi' => 'Id Registrasi',
+            'data' => 'Data',
+            'is_delete' => 'Is Delete',
+            'create_by' => 'Create By',
+            'update_by' => 'Update By',
+            'create_time_at' => 'Create Time At',
+            'update_time_at' => 'Update Time At',
+        ];
+    }
+
+}
---
+++ models/Registrasi.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "registrasi".
+ *
+ * @property int $id_registrasi
+ * @property int $no_registrasi
+ * @property int $no_rekam_medis
+ * @property string $nama_pasien
+ * @property string $tanggal_lahir
+ * @property int $nik
+ * @property int $create_by
+ * @property string $create_time_at
+ * @property int|null $update_by
+ * @property string|null $update_time_at
+ */
+class Registrasi extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'registrasi';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['update_by', 'update_time_at'], 'default', 'value' => null],
+            [['no_registrasi', 'no_rekam_medis', 'nama_pasien', 'tanggal_lahir', 'nik', 'create_by'], 'required'],
+            [['no_registrasi', 'no_rekam_medis', 'nik', 'create_by', 'update_by'], 'default', 'value' => null],
+            [['no_registrasi', 'no_rekam_medis', 'nik', 'create_by', 'update_by'], 'integer'],
+            [['tanggal_lahir', 'create_time_at', 'update_time_at'], 'safe'],
+            [['nama_pasien'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_registrasi' => 'Id Registrasi',
+            'no_registrasi' => 'No Registrasi',
+            'no_rekam_medis' => 'No Rekam Medis',
+            'nama_pasien' => 'Nama Pasien',
+            'tanggal_lahir' => 'Tanggal Lahir',
+            'nik' => 'Nik',
+            'create_by' => 'Create By',
+            'create_time_at' => 'Create Time At',
+            'update_by' => 'Update By',
+            'update_time_at' => 'Update Time At',
+        ];
+    }
+
+}
